dd9d222c920d4441188001af19a02cb6096594b1
Kevinn.h0 <91503622+thiagokevin@users.noreply.github.com>
2023-02-13 18:49:55 +0000

    tela_grafica.java

--- Tela.java
+++ Tela.java
@@ -1,9 +1,40 @@
-public class Tela
-{
-  public static void main(String[] args)
-  {
-    System.out.println("Ola Mundo");
-  }
+import java.awt.Canvas;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+public class Tela extends Canvas implements Runnable
+{
+	
+	int WIDTH = 480, HEIGTH = 480; 
+	
+	
+	public Tela()
+	{
+		this.setPreferredSize(new Dimension(WIDTH,HEIGTH));
+	}
+	
+	public static void main(String[] args)
+	{
+		Tela tela = new Tela();
+		JFrame frame = new JFrame();
+		
+		frame.add(tela);
+		frame.setTitle("Janela");
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+	
+	
+	@Override
+	public void run() 
+	{
+		
+		
+	}
 
 }
